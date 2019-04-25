66d1d1357f4438c72f77ae28dc35e4d574ef781d
zaky <omarbnzaky@gmail.com>
2019-04-25 15:44:11 +0200

    create form

--- resources/js/components/ContactsComponent.vue
+++ resources/js/components/ContactsComponent.vue
@@ -1,5 +1,28 @@
 <template>
-    <h1>Contacts</h1>
+    <div>
+        <h1>Contacts</h1>
+        <form action="#" @submit.prevent="edit ? updateContact(contact.id) : createContact()">
+            <div class="form-group">
+                <label>Name</label>
+                <input v-model="contact.name" type="text" name="name" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label>Email</label>
+                <input v-model="contact.email" type="email" name="email" class="form-control">
+            </div>
+
+          <div class="form-group">
+                <label>Email</label>
+                <input v-model="contact.phone" type="text" name="phone" class="form-control">
+            </div>
+              <div class="form-group">
+                  <button v-show="!edit" type="submit" class="btn btn-primary"> New contact </button>
+                  <button v-show="edit" type="submit" class="btn btn-primary"> Update contact </button>
+
+              </div>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -7,10 +30,30 @@
     export default {
         data: function(){
 
-            return{}
+            return{
+                edit:true,
+                list:[],
+                contact:{
+                    id:'',
+                    name:'',
+                    email:'',
+                    phone:''
+                }
+            }
         },
         mounted: function(){
             console.log('contacts component loaded..');
+        },
+        methods:{
+            createContact : function(){
+                console.log('create');
+                return;
+            },
+
+            updateContact : function(){
+                console.log('edit');                                                                                                                                                                                                  
+                return;
+            },
         }
     }
 
